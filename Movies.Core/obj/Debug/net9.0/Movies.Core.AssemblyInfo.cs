eb3118714e292cf25312af4283989a98742d1d97
Nattaintra <nattarintra.thutsanti@hotmail.com>
2025-07-16 22:22:16 +0200

    Refactor: move Data and Migrations int Movies.Data

--- Movies.Core/obj/Debug/net9.0/Movies.Core.AssemblyInfo.cs
+++ Movies.Core/obj/Debug/net9.0/Movies.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0e2e19a1480d4828ae9d7aefaef962762bfee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
